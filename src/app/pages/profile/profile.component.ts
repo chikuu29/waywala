6361dbafbeb6e0f05f7178b0c65c59937ece294a
Gyana1995 <100307598+Gyana1995@users.noreply.github.com>
2023-01-01 14:03:14 +0530

    lipuns work

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -11,25 +11,80 @@ import Swal from 'sweetalert2';
 })
 export class ProfileComponent implements OnInit {
 
-  profile:any
-  email:string
-  constructor(private apiparameter:ApiParameterScript,private appservices:AppService) { }
+  profile: any;
+  email: string;
+  edit: any = 'btn btn-info';
+  update: any = 'btn btn-info d-noun';
+  showtext: any = 'col-sm-9 text-secondary';
+  showtextbox: any = 'col-sm-9 text-secondary d-noun';
+  name: string;
+  phone: number;
+  address: string;
+  constructor(private apiparameter: ApiParameterScript, private appservices: AppService) { }
 
   ngOnInit(): void {
     console.log(this.appservices.authStatus)
-    var apiData ={
-      "select":"*",
-      "projection":`email='${this.appservices.authStatus.email}'`
-    }
-    this.apiparameter.fetchdata('user',apiData).subscribe((res:any)=>{
+   this.fatchdata();
+  }
 
-      console.log(res)
-      if(res.success){
-        this.profile=res['data'][0]
-      }else{
-        Swal.fire("error",res.message,"error")
+  fatchdata(){
+    var apiData = {
+      "select": "*",
+      "projection": `email='${this.appservices.authStatus.email}'`
+    }
+    this.apiparameter.fetchdata('user', apiData).subscribe((res: any) => {
+
+      //console.log(res)
+      if (res.success) {
+        this.profile = res['data'][0]
+        this.name = this.profile.name;
+        this.phone = this.profile.mobile_no;
+        this.address = this.profile.address;
+      } else {
+        Swal.fire("error", res.message, "error")
       }
     })
   }
+
+  updateuser() {
+    this.edit = 'btn btn-info d-noun';
+    this.update = 'btn btn-info';
+    this.showtext = 'col-sm-9 text-secondary d-noun';
+    this.showtextbox = 'col-sm-9 text-secondary';
+  }
+
+   onclickupdatebtn(){
+    var apiData = {
+     'data' : `name='${this.name}' ,mobile_no='${this.phone}',address='${this.address}'`,
+      "projection": `email='${this.profile.email}'`
+    }
+         this.apiparameter.updatedata('user' , apiData ).subscribe((res : any)=>{
+            //console.log(res);
+
+            if(res.success){
+              const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                didOpen: (toast) => {
+                  toast.addEventListener('mouseenter', Swal.stopTimer)
+                  toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+              })
+              
+              Toast.fire({
+                icon: 'success',
+                title: 'Updated  successfully'
+              })
+            }
+
+            this.update = 'btn btn-info d-noun';
+            this.edit = 'btn btn-info';
+            this.showtextbox= 'col-sm-9 text-secondary d-noun';
+            this.showtext = 'col-sm-9 text-secondary';
+            this.fatchdata();
+         });
+   }
+
 
 }
