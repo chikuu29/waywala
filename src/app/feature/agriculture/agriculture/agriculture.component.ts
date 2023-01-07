9716fe11160499ea49c4bcb8864c78eee565a716
SURYANARAYAN BISWAL <cchiku1999@gmail.com>
2023-01-07 11:59:47 +0530

    done

--- src/app/feature/agriculture/agriculture/agriculture.component.ts
+++ src/app/feature/agriculture/agriculture/agriculture.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgxUiLoaderService, SPINNER } from 'ngx-ui-loader';
+import { ApiParameterScript } from 'src/app/script/api-parameter';
 import { AppService } from 'src/app/services/app.service';
 import { AgricultureService } from '../services/agriculture.service';
 
@@ -13,10 +14,14 @@ export class AgricultureComponent implements OnInit {
   SP = SPINNER.threeStrings;
   loadertext: any = ''
 
+  displayBanner:boolean=true
+  allbannerData:any[]=[];
+
   constructor(
     private ngxService: NgxUiLoaderService,
     private _route: Router,
-    private agriculture: AgricultureService
+    private agriculture: AgricultureService,
+    private apiParameterScript: ApiParameterScript
 
   ) { }
 
@@ -25,6 +30,25 @@ export class AgricultureComponent implements OnInit {
 
     this.agriculture.loadertext.subscribe(res => {
       this.loadertext = res;
+    })
+    this.getBannerInfo()
+  }
+
+  getBannerInfo() {
+
+    let apiData = {
+      'select': '*',
+      'projection': `display_section='Agriculture' AND display_banner='Yes'`,
+      'auth': true
+    }
+    this.apiParameterScript.fetchdata('banner', apiData).subscribe((res: any) => {
+      console.log(res);
+      if (res.success && res['data'].length > 0) {
+        this.allbannerData=res['data']
+      }else{
+        this.allbannerData=[]
+      }
+
     })
   }
   navigateTostatusPage() {
