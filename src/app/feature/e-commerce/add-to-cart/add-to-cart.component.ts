d04328ec2e39c325b6d3053042bfea758f111e66
SURYANARAYAN BISWAL <cchiku1999@gmail.com>
2023-03-18 15:30:30 +0530

    Done

--- src/app/feature/e-commerce/add-to-cart/add-to-cart.component.ts
+++ src/app/feature/e-commerce/add-to-cart/add-to-cart.component.ts
@@ -27,27 +27,44 @@ export class AddToCartComponent implements OnInit {
 
   ngOnInit(): void {
     this.imageURL = this.AppService.getAdminApiPath() + "/shop/images/";
-    if (!this.AppService.authStatus) {
+    if (this.AppService.authStatus) {
       var myKart: any = window.localStorage.getItem('myKartData') != null ? JSON.parse(window.localStorage.getItem('myKartData') as any) : [];
-      console.log("MyKart", myKart);
-      _.map(myKart, (object: any) => {
-        object.product_CART_BY_Email = this.AppService.authStatus.email,
-          object['product_CART_Status'] = 'active'
-      })
-      var apiData = {
-        "keyName": encodeURIComponent(JSON.stringify(["product_CART_ID", "product_CART_QUANTITY", "product_CART_BY_Email", "product_CART_Status", "product_CART_CREATED_TIME"])),
-        "multiDataSet": encodeURIComponent(JSON.stringify(myKart))
-      }
-      console.log(apiData);
-      // console.log(decodeURIComponent(apiData.multiDataSet));
-      this.ApiParameterScript.savedata('e_commerce_product_kart', apiData, true).subscribe((res: any) => {
-        console.log(res);
-        if (res.success) {
-          this.getKartInformation()
-          window.localStorage.removeItem('myKartData');
-        }
+      console.log("Local Stroage Data", myKart);
+      if (myKart.length > 0) {
 
-      })
+        this.ApiParameterScript.fetchdata('e_commerce_product_kart', { "select": "product_CART_ID", "projection": `product_CART_BY_Email='${this.AppService.authStatus.email}'` }).subscribe((res: any) => {
+           
+          console.log("res Data",res);
+          
+          if(res.success){
+          console.log(res['data']);
+          myKart=_.differenceBy(myKart, res['data'],"product_CART_ID")
+          _.map(myKart, (object: any) => {
+            object.product_CART_BY_Email = this.AppService.authStatus.email,
+              object['product_CART_Status'] = 'active'
+          })
+          var apiData = {
+            "keyName": encodeURIComponent(JSON.stringify(["product_CART_ID", "product_CART_QUANTITY", "product_CART_BY_Email", "product_CART_Status", "product_CART_CREATED_TIME"])),
+            "multiDataSet": encodeURIComponent(JSON.stringify(myKart))
+          }
+          this.ApiParameterScript.savedata('e_commerce_product_kart', apiData, true).subscribe((res: any) => {
+            // console.log(res);
+            if (res.success) {
+              this.getKartInformation()
+              window.localStorage.removeItem('myKartData');
+            }
+
+          })
+
+        }
+     
+
+
+        })
+
+
+      }
+
     }
     this.getKartInformation();
 
@@ -57,14 +74,14 @@ export class AddToCartComponent implements OnInit {
 
     var sqlQuery = `SELECT p.*, CAST(COALESCE(AVG(pr.product_Rating),0) AS INTEGER) AS product_AVG_Rating, COUNT(pr.product_Rating) AS product_Total_Rating FROM ( SELECT * FROM e_commerce_product ep INNER JOIN e_commerce_product_kart epk ON epk.product_CART_ID = ep.product_Id WHERE ep.product_Live_Status = 'active' AND epk.product_CART_BY_Email = '${this.AppService.authStatus.email}' ) p LEFT JOIN e_commerce_product_rating pr ON p.product_Id = pr.product_Id GROUP BY p.product_Id;`
     this.ApiParameterScript.fetchDataFormQuery(sqlQuery).subscribe((res: any) => {
-      console.log("kart data", res);
+      // console.log("kart data", res);
       if (res.success) {
         res.data.map((data: any) => {
           data['product_Images'] = data.product_Images.split(',');
         })
         this.allKartItem = res['data'];
         this.calculateTotalProductPrice()
-        console.log("allKartItem", this.allKartItem);
+        // console.log("allKartItem", this.allKartItem);
 
       }
 
@@ -117,12 +134,12 @@ export class AddToCartComponent implements OnInit {
       header: 'Confirm',
       icon: 'pi pi-exclamation-triangle',
       accept: () => {
-       
+
 
         console.log("removeProductFormCart", user_CART_ID);
         var apiData = {
           "projection": `user_CART_ID=${user_CART_ID}`,
-    
+
         }
         this.blockUI.start('Remove Item...')
         this.ApiParameterScript.deletedata('e_commerce_product_kart', apiData, false).subscribe((res: any) => {
@@ -133,12 +150,12 @@ export class AddToCartComponent implements OnInit {
             this.eCommerceService.generateCartItemCount.next(true)
           }
           console.log(res);
-    
+
         })
 
       }
     });
-    
+
 
 
   }
