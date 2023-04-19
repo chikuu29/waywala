3dc20122875f58a0afc71c75f9574f383195afae
SURYANARAYAN BISWAL <cchiku1999@gmail.com>
2023-04-20 00:46:13 +0530

    Update search-items.component.ts

--- src/app/feature/e-commerce/search-items/search-items.component.ts
+++ src/app/feature/e-commerce/search-items/search-items.component.ts
@@ -24,7 +24,7 @@ export class SearchItemsComponent implements OnInit {
     this._rout.queryParams.subscribe((res: any) => {
       console.log(res);
       
-      var query = `SELECT p.product_Id,p.product_Name,p.product_Description,p.product_Mrp_Price,p.product_Selling_Price,p.product_Discoute_Percentage,p.product_Category,p.product_Quantity_Available,p.product_Seller_ID,p.product_Images,p.product_Expires,p.product_Created_Date,p.product_Live_Status, CAST(COALESCE(AVG(pr.product_Rating),0)AS INTEGER) AS product_AVG_Rating,COUNT(pr.product_Rating) AS product_Total_Rating FROM (SELECT * FROM e_commerce_product WHERE e_commerce_product.product_Name LIKE '${res.q}%' AND e_commerce_product.product_Live_Status='active' AND e_commerce_product.product_Category='Vegetable') p LEFT JOIN e_commerce_product_rating pr ON p.product_Id = pr.product_Id GROUP BY p.product_Id, p.product_name;`
+      var query = `SELECT p.product_Id,p.product_Name,p.product_Description,p.product_Mrp_Price,p.product_Selling_Price,p.product_Discoute_Percentage,p.product_Category,p.product_Quantity_Available,p.product_Seller_ID,p.product_Images,p.product_Expires,p.product_Created_Date,p.product_Live_Status, CAST(COALESCE(AVG(pr.product_Rating),0)AS INTEGER) AS product_AVG_Rating,COUNT(pr.product_Rating) AS product_Total_Rating FROM (SELECT * FROM e_commerce_product WHERE e_commerce_product.product_Name LIKE '${res.q}%' AND e_commerce_product.product_Live_Status='active') p LEFT JOIN e_commerce_product_rating pr ON p.product_Id = pr.product_Id GROUP BY p.product_Id, p.product_name;`
       this.ApiParameterScript.fetchDataFormQuery(query).subscribe((res: any) => {
         console.log(res);
         res.data.map((data: any) => {
