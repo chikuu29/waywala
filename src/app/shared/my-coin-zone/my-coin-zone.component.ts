eca02b2f9be05e0acc9d39eed12af41863490e95
SURYANARAYAN BISWAL <cchiku1999@gmail.com>
2023-05-27 16:44:09 +0530

    done

--- src/app/shared/my-coin-zone/my-coin-zone.component.ts
+++ src/app/shared/my-coin-zone/my-coin-zone.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { AppService } from 'src/app/services/app.service';
+import { CoinService } from 'src/app/services/coin.service';
 
 @Component({
   selector: 'app-my-coin-zone',
@@ -8,12 +10,19 @@ import { Title } from '@angular/platform-browser';
 })
 export class MyCoinZoneComponent implements OnInit {
 
+
+  authStatus:any;
   constructor(
-    private taitel:Title
+    private taitel:Title,
+    private appService:AppService,
+    private coinSevices:CoinService
   ) { }
 
   ngOnInit(): void {
     this.taitel.setTitle('My Coin Zone')
+    this.authStatus=this.appService.authStatus
+    console.log(this.appService.authStatus);
+    
   }
 
 }
