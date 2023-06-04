4c32ebd82f1a270b1621faa5ebfdd13f776725b6
SURYANARAYAN BISWAL <cchiku1999@gmail.com>
2023-06-04 16:59:34 +0530

    Done

--- src/app/pages/become-agricultue-investigrator/become-agricultue-investigrator.component.ts
+++ src/app/pages/become-agricultue-investigrator/become-agricultue-investigrator.component.ts
@@ -4,6 +4,7 @@ import { Title } from '@angular/platform-browser';
 import moment from 'moment';
 import { BlockUI, NgBlockUI } from 'ng-block-ui';
 import { ApiParameterScript } from 'src/app/script/api-parameter';
+import { AppService } from 'src/app/services/app.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -19,11 +20,9 @@ export class BecomeAgricultueInvestigratorComponent implements OnInit {
     email: new FormControl('', [Validators.required, Validators.email]),
     phone: new FormControl('', [Validators.required]),
     additionalInfo: new FormControl('', [Validators.required]),
-    businessName: new FormControl('', [Validators.required]),
     adharNo: new FormControl('', [Validators.required]),
     addressInfo: new FormControl('', [Validators.required]),
-    sellingCategory: new FormControl<any | null>(null),
-    ceatedTime:new FormControl(moment().format('LLL').toString())
+    ceatedTime: new FormControl(moment().format('LLL').toString())
   })
 
   sellingCategoryOption: any[] =
@@ -33,15 +32,31 @@ export class BecomeAgricultueInvestigratorComponent implements OnInit {
       { name: 'Fashion' },
       { name: 'Other' }
     ]
-
+  sellOnWaywalaContactFormDATA: any = []
   constructor(
     private Title: Title,
     private _formBuilder: FormBuilder,
-    private ApiParameterScript: ApiParameterScript
+    private ApiParameterScript: ApiParameterScript,
+    private appServices: AppService
   ) { }
 
   ngOnInit(): void {
     this.Title.setTitle('Become Our Agriculture Advisor : Solve Agriculture Farming Problem Through Our Website')
+
+    var apiData = {
+      "select": "*",
+      "projection": `email='${this.appServices.authStatus.email}'`,
+      "order":"id"
+    }
+    this.ApiParameterScript.fetchdata("become_agricultural_reviewer_request", apiData).subscribe((res: any) => {
+      console.log(res);
+      if(res.success && res['data'].length>0){
+         this.sellOnWaywalaContactFormDATA=res['data']
+      }else{
+        this.sellOnWaywalaContactFormDATA=[]
+      }
+
+    })
   }
 
   getErrorMessage() {
@@ -50,32 +65,31 @@ export class BecomeAgricultueInvestigratorComponent implements OnInit {
 
   send() {
 
-
-    this.sellOnWaywalaContactForm.value.sellingCategory=JSON.stringify(this.sellOnWaywalaContactForm.value.sellingCategory)    
     var apiData = {
-      "keyName":encodeURIComponent(JSON.stringify(Object.keys(this.sellOnWaywalaContactForm.value))),
-      "multiDataSet":encodeURIComponent(JSON.stringify([this.sellOnWaywalaContactForm.value]))
+      "keyName": encodeURIComponent(JSON.stringify(Object.keys(this.sellOnWaywalaContactForm.value))),
+      "multiDataSet": encodeURIComponent(JSON.stringify([this.sellOnWaywalaContactForm.value]))
     }
-  
-    if(this.sellOnWaywalaContactForm.valid){
+
+    if (this.sellOnWaywalaContactForm.valid) {
       this.blockUI.start('Please Wait...')
-      this.ApiParameterScript.savedata('becomesellercontactform', apiData, true).subscribe((res:any)=>{
+      this.ApiParameterScript.savedata('become_agricultural_reviewer_request', apiData, true).subscribe((res: any) => {
         console.log(res);
         this.blockUI.stop()
-        if(res.success){
-          Swal.fire('Success',res.message,'success').then(()=>{
+        if (res.success) {
+          Swal.fire('Success', res.message, 'success').then(() => {
+            this.ngOnInit()
             this.sellOnWaywalaContactForm.reset()
           })
-        }else{
-          Swal.fire('Success',res.message,'error')
+        } else {
+          Swal.fire('Success', res.message, 'error')
         }
-        
+
       })
 
-    }else{
-      Swal.fire('Please fill All ','','warning')
+    } else {
+      Swal.fire('Please fill All ', '', 'warning')
     }
-    
+
   }
 
 }
