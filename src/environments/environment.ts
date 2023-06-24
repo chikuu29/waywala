159eaab5445c2edb80bf7cacc1c4a2346c25afa9
SURYANARAYAN BISWAL <cchiku1999@gmail.com>
2023-06-24 17:16:04 +0530

    Done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  baseURL:"http://localhost:4200/",
-  baseApiURL:"http://localhost/waywala-api/",
-  baseAdminApiURL:"http://localhost/waywala-admin-api/",
+  baseURL:"https://waywala.com/",
+  baseApiURL:"https://waywala.com/api/",
+  baseAdminApiURL:"https://admin.waywala.com/api/",
   apiKey:"$2y$10$R33dyQxSYwp58Bx97WHjXu2JNIcZ0lllH3kvgjL5wuSP5hnNHiZO6",
   marketPlaceApiKey:"waywala"
 };
